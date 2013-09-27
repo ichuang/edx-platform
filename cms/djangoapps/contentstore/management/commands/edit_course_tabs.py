eb8cc401e841093e1896ffe53b018b57a11aa871
Nick Parlante <nick.parlante@cs.stanford.edu>
2013-09-27 11:36:20 -0700

    Disallowed certain inputs for edit_course_tabs
    Tabs 1 and 2 and type static_tab are disallowed
    Fixed pep8 stuff

--- cms/djangoapps/contentstore/management/commands/edit_course_tabs.py
+++ cms/djangoapps/contentstore/management/commands/edit_course_tabs.py
@@ -7,7 +7,7 @@
 #   ./manage.py cms --settings dev edit_course_tabs --course Stanford/CS99/2013_spring
 # Or via rake:
 #   rake django-admin[edit_course_tabs,cms,dev,"--course Stanford/CS99/2013_spring --delete 4"]
-# 
+#
 from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 from .prompt import query_yes_no
@@ -29,35 +29,45 @@ def do_print(course):
         print count, '"' + item.get('type') + '"', '"' + item.get('name', '') + '"'
         count += 1
 
+
+def preflight(num, typ):
+    'Throws for the disallowed cases.'
+    if num <= 2:
+        raise CommandError('Tabs 1 and 2 cannot be edited')
+    if typ == 'static_tab':
+        raise CommandError('Tabs of type static_tab cannot be edited here (use Studio)')
+
+
 def do_delete(course, num):
-    "Deletes the given tab number (error if num=1)"
-    if num == 1:
-        raise CommandError('Tab number 1 cannot be deleted')
+    'Deletes the given tab number.'
     tabs = course.tabs
-    del tabs[num - 1]  # -1 due to our 1-based indexing
+    preflight(num, tabs[num - 1].get('type', ''))  # -1 due to our 1-based indexing
+    del tabs[num - 1]
+    # Note: if we did delete static_tab, apparently there's some other stuff to delete as well.
     do_save(course)
     print "*Deleted*"
     do_print(course)
 
-def do_insert(course, num, t, name):
-    "Inserts a new tab at the given number (error if num=1)"
-    if num == 1:
-        raise CommandError('Tab number 1 cannot be changed')
-    new_tab = {u'type': unicode(t), u'name': unicode(name)}
+
+def do_insert(course, num, typ, name):
+    'Inserts a new tab at the given number.'
+    preflight(num, typ)
+    new_tab = {u'type': unicode(typ), u'name': unicode(name)}
     tabs = course.tabs
     tabs.insert(num - 1, new_tab)  # -1 as above
     do_save(course)
     print '*Inserted*'
     do_print(course)
 
+
 def do_save(course):
-    "Saves the course back to modulestore."
+    'Saves the course back to modulestore.'
     # This code copied from
     #  ~/edx_all/edx-platform/cms/djangoapps/contentstore/views/tabs.py
     course.save()
     modulestore('direct').update_metadata(course.location, own_metadata(course))
 
-# course.tabs look like this
+# course.tabs looks like this
 # [{u'type': u'courseware'}, {u'type': u'course_info', u'name': u'Course Info'}, {u'type': u'textbooks'},
 # {u'type': u'discussion', u'name': u'Discussion'}, {u'type': u'wiki', u'name': u'Wiki'},
 # {u'type': u'progress', u'name': u'Progress'}]
@@ -66,33 +76,34 @@ def do_save(course):
 class Command(BaseCommand):
     help = """See and edit a course's tabs list.
 The tabs are numbered starting with 1.
-Tab number 1 cannot be changed.
+Tabs 1 and 2 cannot be changed, and tabs of type
+static_tab cannot be edited (use Studio for those).
 """
     # Making these option objects separately, so can refer to their .help below
     create_option = make_option('--course',
-            action='store',
-            dest='course',
-            default=False,
-            help='--course <id> required, e.g. Stanford/CS99/2013_spring')
+                                action='store',
+                                dest='course',
+                                default=False,
+                                help='--course <id> required, e.g. Stanford/CS99/2013_spring')
     delete_option = make_option('--delete',
-            action='store_true',
-            dest='delete',
-            default=False,
-            help='--delete <tab-number>')
+                                action='store_true',
+                                dest='delete',
+                                default=False,
+                                help='--delete <tab-number>')
     insert_option = make_option('--insert',
-            action='store_true',
-            dest='insert',
-            default=False,
-            help='--insert <tab-number> <type> <name>, e.g. 2 "course_info" "Course Info"')
-    
+                                action='store_true',
+                                dest='insert',
+                                default=False,
+                                help='--insert <tab-number> <type> <name>, e.g. 2 "course_info" "Course Info"')
+
     option_list = BaseCommand.option_list + (create_option, delete_option, insert_option)
 
     def handle(self, *args, **options):
         if not options['course']:
             raise CommandError(Command.create_option.help)
-        
+
         course = get_course_by_id(options['course'])
-        
+
         print 'Warning: this command directly edits the list of course tabs in mongo.'
         print 'Tabs before any changes:'
         do_print(course)
@@ -111,4 +122,3 @@ Tab number 1 cannot be changed.
             name = args[2]
             if query_yes_no('Inserting tab {0} "{1}" "{2}" Confirm?'.format(num, typ, name), default='no'):
                 do_insert(course, num, args[1], args[2])
-
